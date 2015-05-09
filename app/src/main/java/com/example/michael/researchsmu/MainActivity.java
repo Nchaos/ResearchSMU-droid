150ef7f05f424e5759d04c8605d0aa7252ac37c6
mdevitt04 <mdevitt@smu.edu>
2015-05-09 08:10:25 -0500

    first commit

---
+++ app/src/main/java/com/example/michael/researchsmu/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.michael.researchsmu;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        populateListView();
+        registerClickCallBack();
+    }
+
+    private void populateListView()
+    {
+        String[] myItems = {"Dedman","Lyle","Cox","Meadows","Simmons"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view, myItems);
+
+        ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setAdapter(adapter);
+    }
+
+    private void registerClickCallBack()
+    {
+        ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id)
+            {
+                TextView textView = (TextView) viewClicked;
+                String message = "Clicked on " + position + " which is " + textView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(MainActivity.this,ResearchOpportunities.class));
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
